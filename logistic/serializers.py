17a60bbf7556c6cb6f8ec7dbb069542b58fc4dd5
gwest-II <gwest0239@gmail.com>
2022-08-09 23:15:10 +0300

    complied commit

---
+++ logistic/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            obj, res = StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults={'stock': stock, 'product': position['product'],
+                          'quantity': position['quantity'], 'price': position['price']}
+            )
+        return stock
